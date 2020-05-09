a712a92f04a7d29865df0fee67efadf60bf30ec0
Denis O <denis@universal-development.com>
2020-05-09 23:01:40 +0300

    Work item

--- uflow-api/src/main/java/com/unidev/uflow/model/WorkItem.java
+++ uflow-api/src/main/java/com/unidev/uflow/model/WorkItem.java
@@ -1,4 +1,32 @@
 package com.unidev.uflow.model;
 
+import com.unidev.polydata.domain.v3.BasicPoly;
+import com.unidev.polydata.domain.v3.BasicPolyList;
+import lombok.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkItem {
+
+    @Builder.Default
+    private BasicPolyList data = BasicPolyList.newList();
+
+    public void addItem(BasicPoly poly) {
+        data.add(poly);
+    }
+
+    public Optional<BasicPoly> fetchPoly(String id) {
+        return data.polyById(id);
+    }
+
+    public List<BasicPoly> fetchData() {
+        return data.getList();
+    }
+
 }
